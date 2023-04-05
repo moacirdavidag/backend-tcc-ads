1c34f3c1adb6a507f57f4ae554696164b2b8268b
Moacir David <moacirdavidag@gmail.com>
2023-04-04 21:21:14 -0300

    Alterando url de conexão com o banco de dados

--- database/mongo.js
+++ database/mongo.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 main().catch(err => console.log(err));
 
 async function main() {
-  if(process.env.API_MODE === "local") {
-    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-  }
-  await mongoose.connect(`${process.env.DB_PRODUCTION_URL}`);
+  await mongoose.connect(process.env.DB_PRODUCTION_URL);
 }
 
 module.exports = mongoose;
