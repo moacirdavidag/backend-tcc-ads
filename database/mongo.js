e4d83c20159db32274c2a52812e8e88aa52b9543
Moacir David <moacirdavidag@gmail.com>
2023-04-11 10:33:50 -0300

    Alterada a configuração de conexão com o banco de dados

--- database/mongo.js
+++ database/mongo.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DB_PRODUCTION_URL);
+  if(process.env.NODE_ENV === "local") {
+    await mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+  } else {
+    await mongoose.connect(process.env.DB_PRODUCTION_URL);
+  }
 }
 
 module.exports = mongoose;
