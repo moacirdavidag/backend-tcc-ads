a0bd875a92d27c7ff008a8d011af0e4ea6d6bea7
Moacir David <moacirdavidag@gmail.com>
2023-06-01 20:49:45 -0300

    Corrigindo erro na conexão do MongoDB Atlas #3

--- database/mongo.js
+++ database/mongo.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 main().catch(err => console.log(err));
 
+const PASSWORD = encodeURIComponent(process.env.DB_PRODUCTION_PASSWORD);
 
 async function main() {
   if(process.env.NODE_ENV === "local") {
     await mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`);
   } else {
-    console.log("Entrou no modo dev");
-    await mongoose.connect(`mongodb+srv://${process.env.DB_PRODUCTION_USERNAME}:${encodeURIComponent(process.env.DB_PRODUCTION_PASSWORD)}@cluster0.iuddyty.mongodb.net/?retryWrites=true&w=majority
+    console.log("Entrou no modo dev" + PASSWORD);
+    await mongoose.connect(`mongodb+srv://${process.env.DB_PRODUCTION_USERNAME}:${PASSWORD})}@cluster0.iuddyty.mongodb.net/?retryWrites=true&w=majority
     `);
   }
 }
