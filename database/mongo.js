bca3ccbb09fa7b86cf448c3f06d8e965292eb157
moacirdavidag <moacirdavidag@gmail.com>
2023-07-12 20:27:04 -0300

    Adicionando try...catch na conexão com o banco de dados

--- database/mongo.js
+++ database/mongo.js
@@ -5,11 +5,16 @@ main().catch(err => console.log(err));
 
 
 async function main() {
-  if(process.env.NODE_ENV === "local") {
-    await mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`);
-  } else {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_PRODUCTION_USERNAME}:${process.env.DB_PRODUCTION_PASSWORD}@cluster0.iuddyty.mongodb.net/?retryWrites=true&w=majority
-    `);
+  try {
+    if(process.env.NODE_ENV === "local") {
+      await mongoose.connect(`mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+    } else {
+      await mongoose.connect(`mongodb+srv://${process.env.DB_PRODUCTION_USERNAME}:${process.env.DB_PRODUCTION_PASSWORD}@cluster0.iuddyty.mongodb.net/?retryWrites=true&w=majority`, {
+        dbName: 'dadosifpb'
+      });
+    }
+  } catch(e) {
+    console.log(e.message);
   }
 }
 
